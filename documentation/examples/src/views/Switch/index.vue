0603620d3f8bb7bf4008abdbf580852cb3e3b2d5
sebastian <seb.thielke@gmail.com>
2025-05-07 17:34:13 +0200

    Switch docspage colors

--- documentation/examples/src/views/Switch/index.vue
+++ documentation/examples/src/views/Switch/index.vue
@@ -17,7 +17,7 @@ const onToggle = () => {
       <section class="flex items-center space-x-2">
         <span>blue</span>
         <!-- <b-switch small :value="value" on-color-class="bg-lava-red" off-color-class="bg-light-blue" @toggle="onToggle" /> -->
-        <BSwitch small :value="value" on-color-class="bg-lava-red" off-color-class="bg-light-blue" @toggle="onToggle" />
+        <BSwitch small :value="value" on-color-class="bg-red-500" off-color-class="bg-blue-500" @toggle="onToggle" />
         <span>red</span>
       </section>
     </ComponentPageSection>
