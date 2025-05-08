79b6c267517f6033ffbb836548df75a688e8d567
sebastian <seb.thielke@gmail.com>
2025-05-08 13:20:13 +0200

    Checkbox docs as first interactive doc page!

---
+++ documentation/examples/src/components/PropControlSelect.vue
@@ -0,0 +1,30 @@
+<template>
+    <PropControlBase type="<T>">
+        <template #control>
+            <div class="text-secondary font-medium text-sm">{{ name }}</div>
+            <BPopSelect
+                :options="options"
+                :selected="value"
+                filterable
+                @select="onSelect"
+            >
+                <template #trigger="{ visible }">
+                    <BButton
+                        :label="value.label"
+                        :soft-focus="visible"
+                        variant="outlineSubtle"
+                        icon-after="chevron-down"
+                    />
+                </template>
+            </BPopSelect>
+        </template>
+    </PropControlBase>
+</template>
+
+<script setup lang="ts">
+import PropControlBase from './PropControlBase.vue';
+import { BButton, BPopSelect, type TPopSelectOption } from '@firstnoodle/bui';
+defineProps<{ value:TPopSelectOption; options:TPopSelectOption[], name:string }>();
+const emit = defineEmits(['select']);
+const onSelect = (option:TPopSelectOption) => emit('select', option);
+</script>
---
+++ documentation/examples/src/components/PropControlString.vue
@@ -0,0 +1,15 @@
+<template>
+    <PropControlBase type="String">
+        <template #control>
+            <div class="text-secondary font-medium text-sm">{{ name }}</div>
+            <BInput :value="value" @change="(str:string) => $emit('change', str)" />
+        </template>
+        <template #notes><slot/></template>
+    </PropControlBase>
+</template>
+
+<script setup lang="ts">
+import PropControlBase from './PropControlBase.vue';
+import { BInput } from '@firstnoodle/bui';
+defineProps<{ value:string; name:string }>();
+</script>
