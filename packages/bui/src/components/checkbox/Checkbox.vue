df1a3b15306780b816f72f97ee8e2be9b5b6cae6
sebastian <seb.thielke@gmail.com>
2025-05-08 13:19:46 +0200

    Checkbox updated

--- packages/bui/src/components/checkbox/Checkbox.vue
+++ packages/bui/src/components/checkbox/Checkbox.vue
@@ -2,47 +2,45 @@
 import CircleIndicator from "./CircleIndicator.vue";
 import DefaultIndicator from "./DefaultIndicator.vue";
 
-withDefaults(
-  defineProps<{
-    button?: boolean;
-    label?: string;
-    type?: "default" | "circle";
-    value?: number;
-    disabled?: boolean;
-  }>(),
-  {
-    button: false,
-    type: "default",
-    value: 0,
-    disabled: false,
-  },
-);
+const { checked, type = "default", indeterminate = false, disabled = false } = defineProps<{
+  label?: string;
+  type?: "default" | "circle";
+  checked: boolean;
+  indeterminate?: false,
+  disabled?: boolean;
+}>()
 
 const emit = defineEmits(["click"]);
 </script>
 
 <template>
   <div
-    class="items-center space-x-2"
+    class="group flex items-center space-x-2"
     :class="{
-      'flex': !button,
-      'inline-flex border rounded-md h-8 px-3': button,
-      'border-action': value && !disabled,
-      'border-default': !value && !disabled,
+      'border-action': checked && !disabled,
+      'border-default': !checked && !disabled,
       'border-subtle cursor-default': disabled,
       'cursor-pointer': !disabled,
     }"
     @click.stop.prevent="!disabled && emit('click')"
   >
-    <DefaultIndicator v-if="type === 'default'" :value="value" :disabled="disabled" />
-    <CircleIndicator v-else :value="value" :disabled="disabled" />
+    <DefaultIndicator
+      v-if="type === 'default'"
+      :value="checked"
+      :disabled="disabled"
+      :indeterminate="indeterminate"
+    />
+    <CircleIndicator
+      v-else
+      :value="checked"
+      :disabled="disabled"
+      :indeterminate="indeterminate"
+    />
     <span
       v-if="label"
       class="block text-sm leading-tight select-none"
       :class="{
-        'text-action': button && value,
-        'text-secondary': button && !value && !disabled,
-        'hover:underline': !button && !disabled,
+        'hover:underline': !disabled,
         'text-tertiary': disabled,
       }"
     >
--- packages/bui/src/components/checkbox/DefaultIndicator.vue
+++ packages/bui/src/components/checkbox/DefaultIndicator.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-defineProps<{ value: number; disabled: boolean }>();
+const { value, disabled = false, indeterminate = false } = defineProps<{ value: boolean; disabled?: boolean; indeterminate?: boolean }>();
 </script>
 
 <template>
   <button
     class="flex items-center justify-center w-4 h-4 border rounded focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-black dark:focus-visible:outline-white"
     :class="{
-      'border-action bg-action': value && !disabled,
-      'bg-primary border-default': !value && !disabled,
-      'border-default bg-secondary': disabled && value,
-      'border-subtle bg-tertiary': disabled && !value,
-      'cursor-default': disabled,
-      'hover:border-action focus:border-actionLightActive focus:outline-hidden': !disabled,
+      'border-action bg-action hover:bg-action-hover hover:border-action-hover cursor-pointer': (value || indeterminate) && !disabled,
+      'bg-primary border-default hover:border-strong hover:bg-secondary cursor-pointer': !value && !disabled,
+      'cursor-default border-default bg-tertiary': disabled,
     }"
   >
-    <svg v-if="value === 1" width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div v-if="indeterminate" class="w-2 bg-white" style="height: 2px" />
+    <svg v-else-if="value" width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M8.41166 0.5L10 2.0417L4.01353 7.5L0 3.57411L1.60557 2.04932L4.02897 4.39988L8.41166 0.5Z" fill="white" />
     </svg>
-    <div v-if="value === 2" class="w-2 bg-primary" style="height: 2px" />
   </button>
 </template>
