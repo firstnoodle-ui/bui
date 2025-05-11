19dc94dcdf506213f0a3ad8f55ed4455b52a60d9
sebastian <seb.thielke@gmail.com>
2025-05-11 21:38:00 +0200

    shadow

--- packages/bui/src/components/popper/PopperContent.vue
+++ packages/bui/src/components/popper/PopperContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow-sm">
+  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow">
     <slot />
   </div>
 </template>
