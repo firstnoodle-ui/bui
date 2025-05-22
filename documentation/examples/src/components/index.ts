08105f6cd13a73f04f8166b043b914fe91f8c683
sebastian <seb.thielke@gmail.com>
2025-05-22 20:46:00 +0200

    DocsComponent barrel file

---
+++ documentation/examples/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as ComponentPage } from "./ComponentPage.vue";
+export { default as ComponentPageSection } from "./ComponentPageSection.vue";
+export { default as EventFlasher } from "./EventFlasher.vue";
+export { default as EventSection } from "./EventSection.vue";
+export { default as PropControlBoolean } from "./PropControlBoolean.vue";
+export { default as PropControlNumber } from "./PropControlNumber.vue";
+export { default as PropControlSelect } from "./PropControlSelect.vue";
+export { default as PropControlString } from "./PropControlString.vue";
