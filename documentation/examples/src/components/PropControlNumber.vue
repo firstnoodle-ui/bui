7cbfe4471de9cbf4e4d77bd0d3829a191524f5c3
sebastian <seb.thielke@gmail.com>
2025-05-22 20:45:45 +0200

    PropControlNumber

---
+++ documentation/examples/src/components/PropControlNumber.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { BInputNumber } from "@firstnoodle/bui";
+import PropControlBase from "./PropControlBase.vue";
+
+defineProps<{ value: number; name: string }>();
+defineEmits<{
+  (e: "change", value: number): void;
+}>();
+</script>
+
+<template>
+  <PropControlBase type="Number">
+    <template #control>
+      <div class="text-secondary font-medium text-sm">
+        {{ name }}
+      </div>
+      <BInputNumber :value="value" @change="(newValue:number) => $emit('change', newValue)" class="w-20" />
+    </template>
+    <template #notes>
+      <slot />
+    </template>
+  </PropControlBase>
+</template>
