168bb30af1b7477f81b11d2df94a3cba7b603525
sebastian <seb.thielke@gmail.com>
2025-05-22 20:50:28 +0200

    Clean FilterChip doc

--- documentation/examples/src/views/FilterChip/index.vue
+++ documentation/examples/src/views/FilterChip/index.vue
@@ -4,7 +4,6 @@ import { ref } from "vue";
 import ComponentPage from "../../components/ComponentPage.vue";
 import ComponentPageSection from "../../components/ComponentPageSection.vue";
 import PropControlBoolean from "../../components/PropControlBoolean.vue";
-import PropControlSelect from "../../components/PropControlSelect.vue";
 import PropControlString from "../../components/PropControlString.vue";
 import EventFlasher from "../../components/EventFlasher.vue";
 import EventSection from "../../components/EventSection.vue";
@@ -27,15 +26,6 @@ const label = ref('Category');
         </EventSection>
         <PropControlBoolean name="Active" :value="isActive" @toggle="isActive = !isActive" />
         <PropControlString name="Label" :value="label" @change="(value:string) => label = value" />
-        <!-- <PropControlBoolean name="Small" :value="isSmall" @toggle="isSmall = !isSmall" />
-        <PropControlBoolean name="Rounded" :value="isRounded" @toggle="isRounded = !isRounded" />
-        <PropControlBoolean name="Loading" :value="isLoading" @toggle="isLoading = !isLoading" />
-        <PropControlBoolean name="Disabled" :value="isDisabled" @toggle="isDisabled = !isDisabled" />
-        <PropControlBoolean name="Focus" :value="isFocused" @toggle="isFocused = !isFocused" />
-        <PropControlSelect name="Icon" clearable :value="selectedIcon" :options="[...icons]" @select="(option:string|undefined) => selectedIcon = option">
-          Will be hidden when <strong>loading=true</strong>
-        </PropControlSelect>
-        <PropControlSelect name="Variant" :value="selectedVariant" :options="variantOptions" @select="(option:string|undefined) => selectedVariant = option" /> -->
       </template>
     </ComponentPageSection>
   </ComponentPage>
