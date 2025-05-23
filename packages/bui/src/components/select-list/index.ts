8080bc75321d6860bca77af9b0b95ffd99a21cb7
sebastian <seb.thielke@gmail.com>
2025-05-23 08:40:02 +0200

    SelectList dropped

---
+++ packages/bui/src/components/select-list/index.ts
@@ -0,0 +1,1 @@
+export { default as BSelectList } from "./SelectList.vue";
---
+++ packages/bui/src/components/select-list/types.ts
@@ -0,0 +1,41 @@
+import { type TIcon } from "../types";
+
+const states = ['error', 'idle', 'loading', 'match', 'no match'] as const;
+export type SelectListState = typeof states[number];
+
+const selectOptionVariants = ['checkbox', 'deletable', 'single'] as const;
+export type SelectOptionVariant = typeof selectOptionVariants[number];
+
+export type SelectListOption = {
+    id?: string|number
+    icon?: TIcon,
+    label: string
+    disabled?: boolean
+    suffix?: string|number
+    value?: any
+    handler?: (option:SelectListOption) => void
+}
+
+export type SelectListFilter<T> = {
+    id?: string|number
+    name: string
+    disabled: boolean
+    execute: (record:T) => boolean
+    count?: number
+}
+
+export type SelectListProps<T> = {
+    identifier?: keyof T,
+    errorMessage?: string,
+    filters?: SelectListFilter<T>[]
+    loading?: boolean
+    options: T[]
+    placeholder?: string
+    rootClass?: string
+    sameWidthAsTrigger?: boolean
+    searchDebounce?: number
+    searchFunction?: Function | null
+    selectAll?: boolean
+    selected: T|null|T[]
+    showSelection?: boolean
+}
