598c55475cb848f4b58cab24d10281ad1a95a133
sebastian <seb.thielke@gmail.com>
2025-05-27 23:05:36 +0200

    Focus style

--- packages/bui/src/components/toggle-button/ToggleButton.vue
+++ packages/bui/src/components/toggle-button/ToggleButton.vue
@@ -47,17 +47,17 @@ const buttonClasses = computed(() => {
           "bg-blue-100 hover:bg-blue-200 focus-visible:bg-blue-200 active:bg-blue-300",
           "border border-action hover:border-action-hover focus-visible:border-action-hover active:border-action-active",
           "text-action hover:text-action-hover focus-visible:text-action-hover active:text-action-active",
-          "dark:bg-blue-800/25 dark:hover:bg-blue-800/50 focus-visible:bg-blue-800/50 dark:active:bg-blue-800/75",
-          "dark:border-action dark:hover:border-action-hover focus-visible:border-action-hover dark:active:border-action-active",
-          "dark:text-action dark:hover:text-action-hover focus-visible:text-action-hover dark:active:text-action-active",
+          "dark:bg-blue-800/25 dark:hover:bg-blue-800/50 dark:focus-visible:bg-blue-800/50 dark:active:bg-blue-800/75",
+          "dark:border-action dark:hover:border-action-hover dark:focus-visible:border-action-hover dark:active:border-action-active",
+          "dark:text-action dark:hover:text-action-hover dark:focus-visible:text-action-hover dark:active:text-action-active",
       ]
       : [
           "hover:bg-slate-100 focus-visible:bg-slate-100 active:bg-slate-200",
           "border border-stone-300 hover:border-stone-400 focus-visible:border-stone-400 active:border-stone-500",
           "text-slate-700 hover:text-slate-800 focus-visible:text-slate-800 active:text-slate-900",
-          "dark:hover:bg-neutral-700 focus-visible:bg-neutral-700 dark:active:bg-neutral-600",
-          "dark:border-stone-700 dark:hover:border-stone-600 focus-visible:border-stone-600 dark:active:border-stone-500",
-          "dark:text-slate-200 dark:hover:text-slate-100 focus-visible:text-slate-100 dark:active:text-white",
+          "dark:hover:bg-neutral-700 dark:focus-visible:bg-neutral-700 dark:active:bg-neutral-600",
+          "dark:border-stone-700 dark:hover:border-stone-600 dark:focus-visible:border-stone-600 dark:active:border-stone-500",
+          "dark:text-slate-200 dark:hover:text-slate-100 dark:focus-visible:text-slate-100 dark:active:text-white",
       ]
     )
   );
