92137deeedfe6feed2833a4101c773f790b8d7fd
sebastian <seb.thielke@gmail.com>
2025-05-30 23:45:48 +0200

    Tag new style (old still left as comment....)

--- packages/bui/src/components/tag/Tag.vue
+++ packages/bui/src/components/tag/Tag.vue
@@ -17,18 +17,32 @@ const emit = defineEmits(["delete"]);
 </script>
 
 <template>
-  <BFlexbox
+  <!-- <BFlexbox
     inline
-    align="center"
-    class="h-8 leading-tight truncate text-sm border rounded-lg border-sand-grey-15 text-secondary bg-sand-grey-40"
+    class="h-8 gap-0 truncate text-sm border rounded-lg border-default text-secondary bg-secondary"
     :class="{
       'px-2': !deletable,
       'pl-2 pr-1': deletable,
     }"
   >
-    <BIcon v-if="icon" :name="icon" class="mr-1" />
-    <slot v-if="$slots.default" />
-    <span v-else class="truncate">{{ label }}</span>
-    <BButton v-if="deletable" small icon="close" class="ml-1" stop-propagation @click="emit('delete')" />
+    <BFlexbox class="gap-2">
+      <BIcon v-if="icon" :name="icon" />
+      <span class="truncate">{{ label }}</span>
+    </BFlexbox>
+    <BButton v-if="deletable" small variant="textSubtle" icon="close" class="ml-1" stop-propagation @click="emit('delete')" />
+  </BFlexbox> -->
+  <BFlexbox
+    inline
+    class="h-6 gap-0 truncate text-sm border border-default rounded-sm text-tertiary bg-secondary"
+    :class="{
+      'px-2': !deletable,
+      'pl-2 pr-1': deletable,
+    }"
+  >
+    <BFlexbox class="gap-1">
+      <BIcon v-if="icon" :name="icon" />
+      <span class="truncate">{{ label }}</span>
+    </BFlexbox>
+    <BButton v-if="deletable" small variant="textSubtle" icon="close" class="ml-0.5" stop-propagation @click="emit('delete')" />
   </BFlexbox>
 </template>
