a97cbae3f6f7af89824ca76584041aada3c1f530
sebastian <seb.thielke@gmail.com>
2025-05-30 23:44:53 +0200

    PopperContent contrast

--- packages/bui/src/components/popper/PopperContent.vue
+++ packages/bui/src/components/popper/PopperContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow">
+  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow-xl">
     <slot />
   </div>
 </template>
