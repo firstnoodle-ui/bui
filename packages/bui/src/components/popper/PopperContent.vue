52b6dcab72ffbd572367a5b82fdc035effa423fb
sebastian <seb.thielke@gmail.com>
2025-06-09 21:56:31 +0200

    PopperContent stronger shadow in dark theme

--- packages/bui/src/components/popper/PopperContent.vue
+++ packages/bui/src/components/popper/PopperContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow-xl">
+  <div class="pointer-events-auto rounded-lg overflow-hidden border border-default bg-primary shadow-xl dark:shadow-black/25">
     <slot />
   </div>
 </template>
