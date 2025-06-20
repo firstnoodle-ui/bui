bd45e45394f4ca4bd73ce19a3d6c028000cd9570
sebastian <seb.thielke@gmail.com>
2025-06-20 14:00:33 +0200

    switch docs

--- documentation/examples/src/views/Switch/index.vue
+++ documentation/examples/src/views/Switch/index.vue
@@ -17,7 +17,7 @@ const onToggle = () => {
       <section class="flex items-center space-x-2">
         <span>blue</span>
         <!-- <b-switch small :value="value" on-color-class="bg-lava-red" off-color-class="bg-light-blue" @toggle="onToggle" /> -->
-        <BSwitch small :value="value" @toggle="onToggle" />
+        <BSwitch small :value="value" @toggle="onToggle" off-color-class="bg-blue-600" on-color-class="bg-red-600" />
         <span>red</span>
       </section>
     </ComponentPageSection>
