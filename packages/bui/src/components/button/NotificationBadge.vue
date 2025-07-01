12d1c100b90331f62275fdc372a791aa0dff1a08
sebastian <seb.thielke@gmail.com>
2025-07-02 00:32:34 +0200

    NotificationBadge

---
+++ packages/bui/src/components/button/NotificationBadge.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ inside?: boolean }>();
+</script>
+
+<template>
+  <div
+    class="absolute"
+    :style="{
+      top: inside ? '0.15em' : '-0.3em',
+      right: inside ? '0.15em' : '-0.3em',
+    }"
+  >
+    <div class="w-3 h-3 bg-orange-500 rounded-full border border-white dark:border-[var(--color-bg-primary)]" />
+  </div>
+</template>
