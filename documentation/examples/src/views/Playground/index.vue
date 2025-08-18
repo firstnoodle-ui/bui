d203cd7620029ef4297bddc6e12c2b34f8f8842b
sebastian <seb.thielke@gmail.com>
2025-08-18 11:54:00 +0200

    Cleared the Playground

--- documentation/examples/src/views/Playground/index.vue
+++ documentation/examples/src/views/Playground/index.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
-import { BButton, BFlexbox, BTab } from "@firstnoodle/bui";
-
-const tabType = "pill";
 </script>
 
 <template>
-  <main class="p-4">
-    <section class="mb-8">
-      Sketching out Term view
-    </section>
-    <BFlexbox justify="end">
-      <BButton variant="destructive" icon="trash" label="Delete" />
-    </BFlexbox>
-    <BFlexbox>
-      <BTab :type="tabType" icon="calendar" name="Dates" :selected="true" />
-      <BTab :type="tabType" icon="paper" name="Files" :selected="false" />
-      <BTab :type="tabType" icon="authority" name="Teachers" :selected="false" />
-      <BTab :type="tabType" icon="team" name="Students" :selected="false" />
-    </BFlexbox>
-  </main>
+    <div class="max-w-3xl h-full">
+        Go Play
+    </div>
 </template>
