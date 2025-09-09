d38a65ad362566f117faa859c57292cc3ec0246a
sebastian <seb.thielke@gmail.com>
2025-09-09 16:41:10 +0200

    Switch has readonly mode

--- documentation/examples/src/views/Switch/index.vue
+++ documentation/examples/src/views/Switch/index.vue
@@ -5,18 +5,24 @@ import ComponentPage from "../../components/ComponentPage.vue";
 
 const value = ref(false);
 
-const onToggle = () => {
-  value.value = !value.value;
-};
+const onToggle = () => value.value = !value.value;
 </script>
 
 <template>
   <ComponentPage title="Switch">
-    <section class="flex items-center space-x-2">
-      <span>blue</span>
-      <!-- <b-switch small :value="value" on-color-class="bg-lava-red" off-color-class="bg-light-blue" @toggle="onToggle" /> -->
-      <BSwitch small :value="value" off-color-class="bg-blue-600" on-color-class="bg-red-600" @toggle="onToggle" />
-      <span>red</span>
-    </section>
+    <template #default="{ print }">
+      <section class="flex items-center space-x-2">
+        <span>blue</span>
+        <BSwitch
+          small
+          :value="value"
+          @toggle="
+            onToggle();
+            print(`@toggle: ${value ? 'on' : 'off'}`)
+          "
+        /> 
+        <span>red</span>
+      </section>
+    </template>
   </ComponentPage>
 </template>
--- packages/bui/src/components/switch/Switch.vue
+++ packages/bui/src/components/switch/Switch.vue
@@ -1,52 +1,50 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
-const props = withDefaults(
-  defineProps<{
-    value?: boolean;
-    offColorClass?: string;
-    onColorClass?: string;
-    small?: boolean;
-  }>(),
-  {
-    value: false,
-    offColorClass: "bg-tertiary",
-    onColorClass: "bg-tertiary",
-    small: false,
-  },
-);
-
-const emit = defineEmits<{
-  (e: "toggle", event: Event): void;
+const {
+  value = false,
+  offColorClass = "bg-tertiary",
+  onColorClass = "bg-action",
+  small = false,
+  readonly = false
+} = defineProps<{
+  value?: boolean;
+  offColorClass?: string;
+  onColorClass?: string;
+  small?: boolean;
+  readonly?: boolean;
 }>();
 
-const knobClass = computed(() => {
-  return [props.small ? "h-3 w-3" : "h-5 w-5", props.value ? (props.small ? "translate-x-3" : "translate-x-5") : "translate-x-0"];
-});
-
-const backgroundClass = computed(() => {
-  return [props.small ? "h-4" : "h-6", props.value ? props.onColorClass : props.offColorClass];
-});
-
-const backgroundStyle = computed(() => {
-  return {
-    width: props.small ? "1.75rem" : "2.75rem",
-  };
-});
+const emit = defineEmits<{ (e: "toggle"): void; }>();
+const onToggle = (_e:Event) => {
+  if(readonly) return;
+  emit('toggle');
+}
 </script>
 
 <template>
-  <button
+  <component
+    :is="readonly ? 'div' : 'button'"
     role="checkbox"
-    tabindex="0"
-    aria-checked="false"
-    :class="backgroundClass"
-    class="group relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-hidden focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-black dark:focus-visible:outline-white"
-    :style="backgroundStyle"
-    @click.stop.prevent="emit('toggle', $event)"
-    @keydown.space.prevent="emit('toggle', $event)"
-    @keydown.enter.prevent="emit('toggle', $event)"
+    :tabindex="readonly ? -1 : 0"
+    :aria-checked="value"
+    :class="[
+      small ? 'h-4' : 'h-6',
+      value ? onColorClass : offColorClass,
+      readonly ? 'cursor-default' : 'cursor-pointer focus-visible:outline focus-visible:-outline-offset-2 focus-visible:outline-black dark:focus-visible:outline-white'
+    ]"
+    class="group relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full transition-colors ease-in-out duration-200"
+    :style="{ width: small ? '1.75rem' : '2.75rem' }"
+    @click.stop.prevent="onToggle"
+    @keydown.space.prevent="onToggle"
+    @keydown.enter.prevent="onToggle"
   >
-    <span aria-hidden="true" :class="knobClass" class="inline-block rounded-full bg-primary shadow-sm transform transition ease-in-out duration-200" />
-  </button>
+    <span
+      aria-hidden="true"
+      :class="[
+        small ? 'h-3 w-3' : 'h-5 w-5',
+        value ? (small ? 'translate-x-3' : 'translate-x-5') : 'translate-x-0'
+      ]"
+      class="inline-block rounded-full bg-primary shadow-sm transform transition ease-in-out duration-200" />
+  </component>
 </template>
