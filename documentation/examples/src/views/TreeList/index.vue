5f0b76603625017430db9943e443bc1f04baabae
Lasse Munk <lm@it-kartellet.dk>
2025-09-11 21:52:37 +0200

    action no longer has id field

--- documentation/examples/src/views/TreeList/index.vue
+++ documentation/examples/src/views/TreeList/index.vue
@@ -28,7 +28,6 @@ const createNewItem = (path: TreeNode<OrgUnit>[]) => {
 };
 
 const action: TreeNodeAction<OrgUnit> = {
-  id: "add-new",
   label: "Add new",
   icon: "plus",
   handler: createNewItem,
--- packages/bui/src/components/tree-list/types.ts
+++ packages/bui/src/components/tree-list/types.ts
@@ -3,7 +3,6 @@ import type { TIcon } from "../types";
 // define a list of actions that can somehow be added to each node
 
 export type TreeNodeAction<T> = {
-  id: string | number;
   label: string;
   icon?: TIcon;
   handler: (path: TreeNode<T>[]) => void;
