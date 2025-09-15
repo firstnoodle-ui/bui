7cb14588daf60d1fb9c54ec9f52f10e10064eb39
sebastian <seb.thielke@gmail.com>
2025-09-15 10:02:50 +0200

    Dialog cleanup

--- packages/bui/src/components/dialog/Dialog.vue
+++ packages/bui/src/components/dialog/Dialog.vue
@@ -4,21 +4,18 @@ import { onMounted, ref } from "vue";
 import { BButton, BFadeInUp, BScreenOverlay, BVerticalLayout, BWindowFrame } from "../";
 import { useEscapeKey } from "../../composables";
 
-const props = withDefaults(
-  defineProps<{
-    closeable?: boolean;
-    overlayType?: TOverlayType;
-    target?: string;
-    title?: string;
-  }>(),
-  {
-    closeable: true,
-    overlayType: "default",
-    target: "#modals",
-  },
-);
+const {
+  closeable = true,
+  overlayType = "default",
+  target = "#modals",
+} = defineProps<{
+  closeable?: boolean;
+  overlayType?: TOverlayType;
+  target?: string;
+  title?: string;
+}>();
 
-const emit = defineEmits(["close"]);
+const emit = defineEmits(["close", "open"]);
 
 const show = ref(false);
 const windowRef = ref<typeof BWindowFrame>();
@@ -27,31 +24,27 @@ const windowRef = ref<typeof BWindowFrame>();
 onMounted(() => (show.value = true));
 
 const close = () => (show.value = false);
-const onClose = () => props.closeable && close();
-props.closeable && useEscapeKey(onClose);
-
-const onTransitionAfterEnter = () => {
-
-};
+const onClose = () => closeable && close();
+closeable && useEscapeKey(onClose);
 
 defineExpose({ close });
 </script>
 
 <template>
-  <Teleport :to="props.target">
+  <Teleport :to="target">
     <BScreenOverlay :show="show" :type="overlayType" class="p-2 md:pt-32" @click="onClose" @close="emit('close')">
-      <BFadeInUp @transition-after-enter="onTransitionAfterEnter">
+      <BFadeInUp @transition-after-enter="emit('open')">
         <BWindowFrame v-show="show" ref="windowRef" class="h-64 max-w-2xl md:w-1/2 md:max-w-lg p-4 md:px-8">
           <BVerticalLayout>
             <template #header>
               <header class="flex items-center justify-between w-full h-12">
-                <h4 v-if="props.title">
-                  {{ props.title }}
+                <h4 v-if="title">
+                  {{ title }}
                 </h4>
                 <h4 v-else>
                   <slot name="title" />
                 </h4>
-                <BButton v-if="props.closeable" small icon="close" variant="textSubtle" @click="onClose" />
+                <BButton v-if="closeable" small icon="close" variant="textSubtle" @click="onClose" />
               </header>
             </template>
             <template #main>
