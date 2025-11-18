18393ec5535a8bfa4c758914f448eb1957c89336
sebastian <seb.thielke@gmail.com>
2025-11-18 09:35:33 +0100

    Tag docs

--- documentation/examples/src/views/Tag/index.vue
+++ documentation/examples/src/views/Tag/index.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
-import { BFlexbox, BTag } from "@firstnoodle-ui/bui";
-import ComponentPage from "../../components/ComponentPage.vue";
+import type { TIcon } from "@firstnoodle-ui/bui";
+import { BFlexbox, BTag, icons } from "@firstnoodle-ui/bui";
+import { ref } from "vue";
+import {
+  ComponentPage,
+  PropControlBoolean,
+  PropControlSelect,
+  PropControlString,
+} from "../../components";
+
+const isDeletable = ref(false);
+const label = ref("Claude Sonnet");
+const selectedIcon = ref<string | undefined>("agent");
 </script>
 
 <template>
   <ComponentPage>
-    <BFlexbox class="gap-2">
-      <BTag label="ChatGPT 40." @delete="console.log('delete!')" />
-      <BTag icon="venn" label="Mixed" @delete="console.log('delete!')" />
-    </BFlexbox>
+    <template #default="{ print }">
+      <BFlexbox class="gap-2">
+        <BTag
+          :icon="(selectedIcon as TIcon)"
+          :label="label"
+          :deletable="isDeletable"
+          @delete="print('@delete!')"
+        />
+      </BFlexbox>
+    </template>
+    <template #controls>
+      <PropControlSelect name="Icon" clearable :value="selectedIcon" :options="[...icons]" @select="(option:string|undefined) => selectedIcon = option" />
+      <PropControlBoolean name="Deletable" :value="isDeletable" @toggle="isDeletable = !isDeletable">
+        Deletable
+      </PropControlBoolean>
+      <PropControlString name="Label" :value="label" @change="(value:string) => label = value" />
+    </template>
   </ComponentPage>
 </template>
