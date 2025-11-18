3f9d4acede17b427cc24b46a6bf81546ad3c6abe
sebastian <seb.thielke@gmail.com>
2025-11-18 09:26:27 +0100

    Switch propcontrols

--- documentation/examples/src/views/Switch/index.vue
+++ documentation/examples/src/views/Switch/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import { BSwitch } from "@firstnoodle-ui/bui";
 import { ref } from "vue";
-import ComponentPage from "../../components/ComponentPage.vue";
+import {
+  ComponentPage,
+  PropControlBoolean,
+  PropControlSelect,
+} from "../../components";
 
 const value = ref(false);
+const isSmall = ref(false);
+const isReadonly = ref(false);
+const colorClasses = ["bg-secondary", "bg-tertiary", "bg-action", "bg-red-500", "bg-purple-400"];
+const offColorClass = ref("bg-tertiary");
+const onColorClass = ref("bg-action");
 
 const onToggle = () => value.value = !value.value;
 </script>
@@ -14,8 +23,11 @@ const onToggle = () => value.value = !value.value;
       <section class="flex items-center space-x-2">
         <span>blue</span>
         <BSwitch
-          small
+          :small="isSmall"
+          :readonly="isReadonly"
           :value="value"
+          :off-color-class="offColorClass"
+          :on-color-class="onColorClass"
           @toggle="
             onToggle();
             print(`@toggle: ${value ? 'on' : 'off'}`)
@@ -23,6 +35,23 @@ const onToggle = () => value.value = !value.value;
         />
         <span>red</span>
       </section>
+    </template>
+    <template #controls>
+      <PropControlBoolean name="Value" :value="value" @toggle="value = !value">
+        On/Off
+      </PropControlBoolean>
+      <PropControlBoolean name="Small" :value="isSmall" @toggle="isSmall = !isSmall">
+        Small
+      </PropControlBoolean>
+      <PropControlBoolean name="Readonly" :value="isReadonly" @toggle="isReadonly = !isReadonly">
+        Readonly
+      </PropControlBoolean>
+      <PropControlSelect name="onColorClass" :value="onColorClass" :options="colorClasses" @select="(option: string) => onColorClass = option">
+        onColorClass
+      </PropControlSelect>
+      <PropControlSelect name="offColorClass" :value="offColorClass" :options="colorClasses" @select="(option: string) => offColorClass = option">
+        offColorClass
+      </PropControlSelect>
     </template>
   </ComponentPage>
 </template>
