9b17b00a0b5f741c42620a4710b4843f1c3b67e6
sebastian <seb.thielke@gmail.com>
2025-11-19 11:10:29 +0100

    Input has size and variant props

--- documentation/examples/src/views/Input/index.vue
+++ documentation/examples/src/views/Input/index.vue
@@ -11,9 +11,13 @@ import {
 
 const inputRef = ref<typeof BInput>();
 
+const showInlineControls = ref(false);
 const str = ref("");
 const placeholder = ref("Write something");
-const selectedIcon = ref<string | undefined>();
+const selectedIcon = ref<string | undefined>("admin");
+const size = ref<"default" | "small">("small");
+const variant = ref<"border" | "fill">("border");
+const multiline = ref(false);
 
 const matchCase = ref(false);
 const matchDiacritics = ref(false);
@@ -25,31 +29,40 @@ const clearable = ref(false);
 <template>
   <ComponentPage>
     <template #default="{ print }">
-      <BInput
-        ref="inputRef"
-        :value="str"
-        :icon="(selectedIcon as TIcon)"
-        :placeholder="placeholder"
-        :clearable="clearable"
-        @change="(newValue:string) => {
-          str = newValue;
-          print(`change: ${newValue}`);
-        }"
-        @enter="print('enter key')"
-        @clear="print('clear')"
-      >
-        <template #inline-controls>
-          <BFlexbox class="gap-1">
-            <BToggleButton tooltip="Match case" icon="match-case" :active="matchCase" @click="matchCase = !matchCase; inputRef?.focus()" />
-            <BToggleButton tooltip="Match full word" icon="match-full-word" :active="matchFullWord" @click="matchFullWord = !matchFullWord; inputRef?.focus()" />
-            <BToggleButton tooltip="Match diacritics" icon="match-diacritics" :active="matchDiacritics" @click="matchDiacritics = !matchDiacritics; inputRef?.focus()" />
-          </BFlexbox>
-        </template>
-      </BInput>
+      <div class="w-full flex gap-2">
+        <BInput
+          ref="inputRef"
+          :value="str"
+          :icon="(selectedIcon as TIcon)"
+          :placeholder="placeholder"
+          :clearable="clearable"
+          :size="size"
+          :variant="variant"
+          :multiline="multiline"
+          @change="(newValue:string) => {
+            str = newValue;
+            print(`change: ${newValue}`);
+          }"
+          @enter="print('enter key')"
+          @clear="str = ''; print('clear')"
+        >
+          <template v-if="showInlineControls" #inline-controls>
+            <BFlexbox class="gap-1">
+              <BToggleButton tooltip="Match case" icon="match-case" :active="matchCase" @click="matchCase = !matchCase; inputRef?.focus()" />
+              <BToggleButton tooltip="Match full word" icon="match-full-word" :active="matchFullWord" @click="matchFullWord = !matchFullWord; inputRef?.focus()" />
+              <BToggleButton tooltip="Match diacritics" icon="match-diacritics" :active="matchDiacritics" @click="matchDiacritics = !matchDiacritics; inputRef?.focus()" />
+            </BFlexbox>
+          </template>
+        </BInput>
+      </div>
     </template>
     <template #controls>
+      <PropControlBoolean name="Show inline controls" :value="showInlineControls" @toggle="showInlineControls = !showInlineControls" />
       <PropControlString name="Placeholder" :value="placeholder" @change="(value:string) => placeholder = value" />
       <PropControlSelect name="Icon" clearable :value="selectedIcon" :options="[...icons]" @select="(option:string|undefined) => selectedIcon = option" />
+      <PropControlSelect name="Size" :value="size" :options="['default', 'small']" @select="(option:'default'|'small') => size = option" />
+      <PropControlSelect name="Variant" :value="variant" :options="['border', 'fill']" @select="(option:'border'|'fill') => variant = option" />
+      <PropControlBoolean name="Multiline" :value="multiline" @toggle="multiline = !multiline" />
       <PropControlBoolean name="Clearable" :value="clearable" @toggle="clearable = !clearable" />
     </template>
   </ComponentPage>
--- packages/bui/src/components/input/Input.vue
+++ packages/bui/src/components/input/Input.vue
@@ -4,25 +4,30 @@ import type { TIcon } from "../types";
 import { onMounted, ref } from "vue";
 import { BButton, BIcon } from "../";
 
+// size and variant props
+
 const {
   disabled = false,
   icon,
   multiline = false,
   placeholder = "Write",
   autoFocus = false,
+  size = "default",
+  variant = "border",
 } = defineProps<{
   disabled?: boolean;
   clearable?: boolean;
   icon?: TIcon;
   multiline?: boolean;
   placeholder?: string;
-  small?: boolean;
+  size?: "default" | "small";
   value: string;
+  variant?: "border" | "fill";
   inputType?: InputTypeHTMLAttribute;
   autoFocus?: boolean;
 }>();
 
-const emit = defineEmits(["change", "clear", "enter"]);
+const emit = defineEmits(["change", "enter"]);
 
 const textareaRef = ref<HTMLTextAreaElement>();
 const inputRef = ref<HTMLInputElement>();
@@ -64,23 +69,30 @@ defineExpose({ focus });
 
 <template>
   <div
-    class="relative overflow-hidden inline-flex items-center h-8 text-sm leading-tight rounded-lg border focus:shadow-sm-inner focus:outline-hidden focus-within:border-action"
+    class="relative flex-1 w-full overflow-hidden inline-flex items-center text-sm rounded-lg border focus:shadow-sm-inner focus:outline-hidden focus-within:border-action"
     :class="{
-      'bg-secondary border-weak text-secondary': disabled,
-      'bg-primary border-default text-primary': !disabled,
-      'pl-8': icon,
-      'pl-3': !icon,
-      'pr-1': clearable,
-      'pr-0.5': $slots['inline-controls'],
-      'pr-3': !clearable && !$slots['inline-controls'],
+      'bg-primary border-default text-primary': variant === 'border' && !disabled,
+      'bg-secondary border-weak text-secondary': variant === 'border' && disabled,
+      'bg-tertiary border-transparent text-primary': variant === 'fill' && !disabled,
+      'bg-tertiary border-weak text-secondary': variant === 'fill' && disabled,
+      'h-8': size === 'default' && !multiline,
+      'pl-2': size === 'default' && !icon,
+      'h-6': size === 'small' && !multiline,
+      'pl-1.5': size === 'small' && !icon,
+      'pl-0': icon,
+      'pr-1': clearable && value.length && size === 'default',
+      'pr-0.5': clearable && value.length && size === 'small',
+      'pr-0': !(clearable && value.length),
     }"
   >
     <div
       v-if="icon"
-      class="absolute top-0 left-0 pr-1 pl-2 h-full flex rounded-lg bg-transparent cursor-pointer"
+      class="flex-none h-full flex rounded-lg bg-transparent cursor-pointer"
       :class="{
         'items-start py-2': multiline,
         'items-center': !multiline,
+        'px-2': size === 'default',
+        'px-1.5': size === 'small',
       }"
     >
       <BIcon :name="icon" class="text-secondary" />
@@ -92,7 +104,11 @@ defineExpose({ focus });
       :disabled="disabled"
       :value="value"
       :placeholder="placeholder"
-      class="flex-1 py-2 w-full text-sm leading-tight bg-transparent focus:outline-hidden"
+      class="flex-1 py-2 w-full bg-transparent focus:outline-hidden"
+      :class="{
+        'text-xs': size === 'small',
+        'text-sm': size === 'default',
+      }"
       @input.stop.prevent="onInput"
     />
     <input
@@ -103,16 +119,30 @@ defineExpose({ focus });
       :type="inputType"
       :value="value"
       :placeholder="placeholder"
-      class="flex-1 min-w-0 text-sm leading-tight bg-transparent focus:outline-hidden"
+      class="flex-1 min-w-0 bg-transparent focus:outline-hidden"
+      :class="{
+        'text-xs': size === 'small',
+        'text-sm': size === 'default',
+      }"
       @change.stop.prevent
       @keydown.enter.stop.prevent="onEnter"
       @keyup.enter.stop.prevent
       @blur="onBlur"
       @input.stop.prevent="onInput"
     >
-    <section v-if="$slots['inline-controls']" class="px-1">
+    <section v-if="$slots['inline-controls']" class="flex-none px-1">
       <slot name="inline-controls" />
     </section>
-    <BButton v-if="clearable && value.length" small icon="close" variant="textSubtle" class="flex-none" @click="emit('clear')" />
+    <BButton
+      v-if="clearable && value.length"
+      small
+      icon="close"
+      variant="textSubtle"
+      class="flex-none hidden"
+      @click="
+        focus();
+        emit('change', '');
+      "
+    />
   </div>
 </template>
