04c6d49d939b3a8fcdfb390d6228d1bc04e35793
sebastian <seb.thielke@gmail.com>
2025-12-10 12:14:39 +0100

    SegmentedControls placeholder

---
+++ packages/bui/src/components/segmented-controls/SegmentedControls.vue
@@ -0,0 +1,18 @@
+<template>
+  TODO
+  <!-- From CT -->
+  <!-- <BFlexbox class="gap-0.5 p-1 rounded-lg bg-secondary">
+    <button
+    class="cursor-pointer h-6 px-2 flex items-center gap-2 rounded"
+    :class="{
+    'text-muted bg-pink-400 hover:bg-white hover:text-tertiary':
+    userSettings.themeId !== ThemeEnum.LIGHT,
+    'bg-primary shadow-xs text-primary': userSettings.themeId === ThemeEnum.LIGHT,
+    }"
+    @click="setUserSetting({ themeId: ThemeEnum.LIGHT })"
+    >
+    <BIcon name="sun" />
+    <p class="text-xs">Light</p>
+    </button>
+  </BFlexbox> -->
+</template>
