6a108ed83772676fd1d5e4f716bcf8c2f2a7f63e
sebastian <seb.thielke@gmail.com>
2025-12-10 12:03:55 +0100

    Exporting FormattingButton, Divider, and InsertLink

--- packages/bui/src/components/text-editor/components/index.ts
+++ packages/bui/src/components/text-editor/components/index.ts
@@ -1,3 +1,6 @@
+export { default as Editor } from "./Editor.vue";
 export { default as FormattingButton } from "./FormattingButton.vue";
+export { default as FormattingMenuDivider } from "./FormattingMenuDivider.vue";
+export { default as InsertLink } from "./InserLink.vue";
 export { default as InsertLinkDialog } from "./InsertLinkDialog.vue";
 export { default as InsertTextDialog } from "./InsertTextDialog.vue";
--- packages/bui/src/components/text-editor/index.ts
+++ packages/bui/src/components/text-editor/index.ts
@@ -1,1 +1,6 @@
+export {
+  FormattingButton as BFormattingButton,
+  FormattingMenuDivider as BFormattingMenuDivider,
+  InsertLink as BInsertLink,
+} from "./components";
 export { default as BTextEditor } from "./TextEditor.vue";
